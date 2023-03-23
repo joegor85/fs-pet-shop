67733d919430d97a13b8c65d12ecc7ff59287f8d
joegor85 <joe.gor85@gmail.com>
2023-03-23 17:36:34 -0500

    added bonus on part 2 for POST

--- httpServer.js
+++ httpServer.js
@@ -2,7 +2,9 @@ const fs = require("fs");
 const http = require("http");
 const port = process.env.PORT || 8000;
 
+// Tell the server what to do when it gets a request
 var handleRequest = http.createServer((req, res) => {
+  console.log();
   if (req.method == "GET") {
     let urlSplit = req.url.split("/");
     //console.log(index);
@@ -17,7 +19,7 @@ var handleRequest = http.createServer((req, res) => {
 
     function readSomething(data) {
       let index = urlSplit[2];
-      console.log(data);
+      //console.log(data);
       if (req.url == "/pets") {
         res.writeHead(200, { "Content-Type": "application/json" });
         res.write(JSON.stringify(data));
@@ -32,11 +34,43 @@ var handleRequest = http.createServer((req, res) => {
         res.end("Not Found");
       }
     }
+  } else if (req.method == "POST") {
+    let string = "";
+    req.on("data", (chunk) => {
+      string += chunk;
+    });
+    req.on("end", () => {
+      let parsedString = JSON.parse(string);
+      if (
+        isNaN(parsedString.age) ||
+        parsedString.name == "" ||
+        parsedString.kind == ""
+      ) {
+        res.writeHead(400, { "Content-Type": "text/plain" });
+        res.end("Bad Request");
+        return;
+      }
+      //console.log(JSON.parse(string));
+      fs.readFile("pets.json", "UTF-8", (error, data2) => {
+        let parsedString = JSON.parse(string);
+        parsedString.age = Number(parsedString.age);
+        console.log(parsedString);
+        if (error) {
+          console.error(error);
+        } else {
+          writeJSON(data2, parsedString);
+        }
+      });
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.write(string);
+      res.end("Received POST data\n");
+    });
   } else {
     console.error("Please use the correct method");
   }
 });
 
+// Make server listen
 handleRequest.listen(port, (error) => {
   if (error) {
     console.error(error);
@@ -44,3 +78,9 @@ handleRequest.listen(port, (error) => {
     console.log(`Server running on ${port}`);
   }
 });
+
+function writeJSON(data, string) {
+  let parsedData = JSON.parse(data);
+  parsedData.push(string);
+  console.log(parsedData);
+}
